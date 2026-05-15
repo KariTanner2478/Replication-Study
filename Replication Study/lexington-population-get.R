ca13451d4027d962c551b7d9be2fcef04a7e7d7f
KariTanner2478 <backupforssint@gmail.com>
2026-05-15 16:37:36 -0700

    Delete Replication Study directory

--- Replication Study/lexington-population-get.R
+++
@@ -1,5 +0,0 @@
-library(tidycensus)
-library(tidyverse)
-census_api_key("1260c81a6165bb09504674f370c30ce8743aff14", install=TRUE)
-
-LexingtonPop_2020 <- read.csv("C:/Users/karit/Downloads/nhgis0002_ds258_2020_county.csv")
